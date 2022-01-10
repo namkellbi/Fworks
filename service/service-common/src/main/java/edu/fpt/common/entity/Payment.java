29167f50f4704ddf706879f368879ed0f03de64b
nhnam13 <nhnam13@cmcglobal.vn>
2022-01-10 16:33:08 +0700

    add entity for project

--- service/service-common/src/main/java/edu/fpt/common/entity/Payment.java
+++ service/service-common/src/main/java/edu/fpt/common/entity/Payment.java
@@ -1,4 +1,9 @@
 package edu.fpt.common.entity;
-
+//phát triển đơn giản, chỉ hiển thị thông tin ngân hàng
 public class Payment {
+    private long id;
+    private String paymentType; // cash, bank, momo, visa....
+    private String receiver;
+    private String amount;
+    private String information;
 }
--- service/service-common/src/main/java/edu/fpt/common/entity/Wishlist.java
+++ service/service-common/src/main/java/edu/fpt/common/entity/Wishlist.java
@@ -1,4 +1,57 @@
 package edu.fpt.common.entity;
-
+// thêm các job vào mục yêu thích
 public class Wishlist {
+    private long id;
+    private long postId;
+    private long userId;
+    private boolean status;
+    private boolean isDelete;
+
+    public Wishlist(long id, long postId, long userId, boolean status, boolean isDelete) {
+        this.id = id;
+        this.postId = postId;
+        this.userId = userId;
+        this.status = status;
+        this.isDelete = isDelete;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(long postId) {
+        this.postId = postId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public boolean isDelete() {
+        return isDelete;
+    }
+
+    public void setDelete(boolean delete) {
+        isDelete = delete;
+    }
 }
